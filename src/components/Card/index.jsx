ae537c2e73ec216184a6e1fa0c6e1697c08e6af4
Kayo Silva <Kayohenriquesilva19@gmail.com>
2023-11-22 18:20:25 -0300

    feat: cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -27,48 +27,99 @@ export const Card = ({ item }) => {
   }, [])
 
   const defaultButton =
-    'translate-x-[-700px] h-[120px] w-[100px] border border-neutral-900 bg-stone-50 rounded drop-shadow-md flex justify-center items-center hover:scale-125'
+    'translate-x-[-700px] h-[120px] w-[100px] border border-neutral-900 bg-stone-50 rounded drop-shadow-md flex justify-center items-center transition-all'
 
   function iconSelected(item) {
     switch (item.name) {
       case 'GitHub':
         return (
-          <button id="cardButton" className={`${defaultButton} `}>
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-blue-950 hover:text-neutral-50`}
+          >
             <GithubLogo size={48} />
           </button>
         )
 
       case 'Discord':
         return (
-          <button id="cardButton" className={`${defaultButton} `}>
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-violet-500 hover:text-neutral-50`}
+          >
             <DiscordLogo size={48} />
           </button>
         )
 
       case 'Medium':
         return (
-          <button id="cardButton" className={`${defaultButton} `}>
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-neutral-900 hover:text-neutral-50`}
+          >
             <MediumLogo size={48} />
           </button>
         )
 
       case 'Linkedin':
-        return <LinkedinLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-blue-500 hover:text-neutral-50`}
+          >
+            <LinkedinLogo size={48} />
+          </button>
+        )
 
       case 'Instagram':
-        return <InstagramLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500  hover:text-neutral-50`}
+          >
+            <InstagramLogo size={48} />
+          </button>
+        )
 
       case 'Email':
-        return <GoogleLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-red-700 hover:text-neutral-50`}
+          >
+            <GoogleLogo size={48} />
+          </button>
+        )
 
       case 'Whatsapp':
-        return <WhatsappLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-green-500 hover:text-neutral-50`}
+          >
+            <WhatsappLogo size={48} />
+          </button>
+        )
 
       case 'Youtube':
-        return <YoutubeLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-red-500 hover:text-neutral-50`}
+          >
+            <YoutubeLogo size={48} />
+          </button>
+        )
 
       case 'Twitch':
-        return <TwitchLogo size={48} />
+        return (
+          <button
+            id="cardButton"
+            className={`${defaultButton} hover:bg-purple-500 hover:text-neutral-50`}
+          >
+            <TwitchLogo size={48} />
+          </button>
+        )
     }
   }
 
