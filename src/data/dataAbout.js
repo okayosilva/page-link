74e4457cf390a37db6d0cce5e841df40520c51f0
Kayo Silva <91280312+okayosilva@users.noreply.github.com>
2024-04-02 09:55:35 -0300

    Update dataAbout.js

--- src/data/dataAbout.js
+++ src/data/dataAbout.js
@@ -1,5 +1,5 @@
 const dataAbout = [
-  'Desenvolvedor com mais de 4 anos de experiência na criação de software, com foco principalmente em stacks mobile e front-end. No entanto, também possuo experiência em desenvolvimento full-stack. Meu compromisso sempre é buscar qualidade e excelência em tudo o que realizo. ',
+  'Desenvolvedor com mais de 3 anos de experiência na criação de software, com foco principalmente em stacks mobile e front-end. No entanto, também possuo experiência em desenvolvimento full-stack. Meu compromisso sempre é buscar qualidade e excelência em tudo o que realizo. ',
 ]
 
 export default dataAbout
